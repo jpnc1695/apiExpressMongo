74505dfb96b90299a7c0f1c6231933e9faea61ef
João Paulo N Costa <84605494+jpnc1695@users.noreply.github.com>
2022-09-23 18:03:42 -0300

    Add files via upload
    primeiro commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import app from './src/app.js'
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+  console.log( `Servidor iniciado na porta local: ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
