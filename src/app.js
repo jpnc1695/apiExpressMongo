80c532049b9e754d87af2a8eedb755d2c84cd510
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-09-25 18:02:35 -0300

    .env

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import db from './config/dbConnect.js'
 import cors from 'cors'
 import routes from './routes/index.js'
+require('dotenv').config()
+
 
 db.on("error", console.log.bind(console, 'erro de conexão'))
 db.once("open", ()=> {
